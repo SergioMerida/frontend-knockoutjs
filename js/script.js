ec460546138b51418a3f0410a1950f6104f9119a
Sergio Tabarini <tabarinisergio@gmail.com>
2014-12-16 11:49:28 -0600

    First commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-// Javascript Code.
+function 
+var Person = function(name, children) {
+    this.name = name;
+    this.children = ko.observableArray(children);
+ 
+    this.addChild = function() {
+        this.children.push("New child");
+    }.bind(this);
+}
+ 
+// The view model is an abstract description of the state of the UI, but without any knowledge of the UI technology (HTML)
+var viewModel = {
+    people: [
+        new Person("Annabelle", ["Arnie", "Anders", "Apple"]),
+        new Person("Bertie", ["Boutros-Boutros", "Brianna", "Barbie", "Bee-bop"]),
+        new Person("Charles", ["Cayenne", "Cleopatra"])
+        ],
+    showRenderTimes: ko.observable(false)
+};
+
+ko.applyBindings(viewModel);
+
+var BetterListModel = function () {
+    this.itemToAdd = ko.observable("");
+    this.allItems = ko.observableArray(["Fries", "Eggs Benedict", "Ham", "Cheese"]); // Initial items
+    this.selectedItems = ko.observableArray(["Ham"]);                                // Initial selection
+ 
+    this.addItem = function () {
+        if ((this.itemToAdd() != "") && (this.allItems.indexOf(this.itemToAdd()) < 0)) // Prevent blanks and duplicates
+            this.allItems.push(this.itemToAdd());
+        this.itemToAdd(""); // Clear the text box
+    };
+ 
+    this.removeSelected = function () {
+        this.allItems.removeAll(this.selectedItems());
+        this.selectedItems([]); // Clear selection
+    };
+ 
+    this.sortItems = function() {
+        this.allItems.sort();
+    };
+};
+ 
+ko.applyBindings(new BetterListModel());
+
